1281e640134eb2dda7aea4c4ec08c7b4c23b4b21
Jonathan Cady <joncady@uw.edu>
2019-10-03 23:35:20 -0700

    Updates

---
+++ precache-manifest.50625538b50a2c431bf4ef884f0c9c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7c7862c9c8807340353065f4a97096",
+    "url": "/travel_map/index.html"
+  },
+  {
+    "revision": "b778130d8c90a0bbc147",
+    "url": "/travel_map/static/css/main.c52daac5.chunk.css"
+  },
+  {
+    "revision": "fe180834eb387ac7ffcc",
+    "url": "/travel_map/static/js/2.89e79892.chunk.js"
+  },
+  {
+    "revision": "b778130d8c90a0bbc147",
+    "url": "/travel_map/static/js/main.a19e26a9.chunk.js"
+  },
+  {
+    "revision": "674ea56f8e076b35f49f",
+    "url": "/travel_map/static/js/runtime-main.445fb3eb.js"
+  }
+]);
